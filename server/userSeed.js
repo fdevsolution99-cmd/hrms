7746a2e21991e261f89602a1bcec41aa945f8c6d
Kishorefdevsol <kishore@fdevsol.com>
2026-03-05 10:59:39 +0530

    userSeed

--- server/userSeed.js
+++ server/userSeed.js
@@ -1,10 +1,22 @@
 import User from './models/User.js'
 import bcrypt from 'bcryptjs'
-import connectToDatabase from './db/db.js'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const userRegister = async () => {
-    await connectToDatabase()
     try {
+        console.log("Connecting to MongoDB...");
+        const uri = process.env.MONGODB_URL;
+        await mongoose.connect(uri, { serverSelectionTimeoutMS: 10000 });
+        console.log("MongoDB connected");
+
         const existingUser = await User.findOne({ email: "admin@fdevsol.com" })
         if (existingUser) {
             console.log("Admin user already exists")
